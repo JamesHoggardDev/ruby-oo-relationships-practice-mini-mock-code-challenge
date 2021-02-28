63928d07c528167e805fc71fd928abe5a8eca640
james <jayrh3246@gmail.com>
2021-02-28 18:57:38 -0500

    commit tree 815f76e7f280b54dfc1597e165a8c414bde4142c

--- author.rb
+++ author.rb
@@ -1,1 +1,28 @@
+class Author
+      attr_reader :name
 
+    def initialize(name)
+        @name = name
+    end
+
+    def books
+        Book.all.select{|book| book.author == self}
+    end
+
+    def self.all
+        Author.all
+    end
+
+    def write_book(title, word_count)
+        book = Book.new(title, word_count)
+        book.author = self
+    end
+
+    def total_words
+        self.books.word_count
+    end
+
+    def most_words
+        Book.all.select {|book| book.total_words.max == self}
+    end
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,18 @@
+class Book
+    attr_reader :title, :author , :word_count
+    
+    @@all = []
 
+    def initialize(title, author, word_count)
+        @@all << self
+    end
+
+    def word_count
+        self.word_count
+    end
+
+    def self.all
+        @@all
+    end
+
+end
